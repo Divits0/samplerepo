1f48ed4ba3b6277abd2707a6d3755c9055e55ece
Divit <divitshah5@gmail.com>
2022-08-02 12:11:25 +0530

    First Commit

---
+++ Mathematics.java
@@ -0,0 +1,17 @@
+package com.persistent.mathapp;
+
+public class Mathematics {
+	public int Add(int num1,int num2) {
+		return num1+num2;
+	}
+	
+	public int Sub(int num1,int num2) {
+		return num1-num2;
+	}
+	
+	public double Divide(int num1,int num2) {
+		return num1/num2;
+	}
+}
+
+	
